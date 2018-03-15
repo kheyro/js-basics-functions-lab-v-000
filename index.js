e52c98cb4327aa76bcff55d1708a0f34e6169329
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 10:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ function calculatesFarePrice(start, destination) {
     return 0
   } else if (400 < d && d <= 2000) {
     return d * 0.2
+  } else if (2000< d && d <= 2500) {
+    return 25
+  } else {
+    return 'cannot travel that far'
   }
-
 }
